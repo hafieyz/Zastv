571749c8ebb84d6da2819d5d46c433bcf94035e3
Hafiz Amirudin <hafieyzamirudin@gmail.com>
2024-08-04 02:05:04 +0800

    Update source.js

--- source.js
+++ source.js
@@ -1,6 +1,6 @@
 const videoSources = [
     { label: 'DASH', type: 'mpd', url: 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd' },
-    { label: 'HLS', type: 'm3u8', url: 'https://d25tgymtnqzu8s.cloudfront.net/smil:berita/playlist.m3u8?id=5' }
+    { label: 'HLS', type: 'm3u8', url: 'https://d25tgymtnqzu8s.cloudfront.net/smil:berita/playlist.m3u8?id=5' },
     { label: 'DASH', type: 'mpd', url: 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd' },
     { label: 'HLS', type: 'm3u8', url: 'https://d25tgymtnqzu8s.cloudfront.net/smil:berita/playlist.m3u8?id=5' }
     // Add more video sources here
