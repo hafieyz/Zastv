0b8fef539181a3187d6f465f6df64e8f2b7a6038
Hafiz Amirudin <hafieyzamirudin@gmail.com>
2024-08-04 01:42:31 +0800

    Create source.js

---
+++ source.js
@@ -0,0 +1,5 @@
+const videoSources = [
+    { label: 'Sintel (DASH)', type: 'mpd', url: 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd' },
+    { label: 'Sample Video (HLS)', type: 'm3u8', url: 'https://d25tgymtnqzu8s.cloudfront.net/smil:berita/playlist.m3u8?id=5' }
+    // Add more video sources here
+];
